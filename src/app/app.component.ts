cba5aaa5e746081e1e86e985d8919b715ad673f3
Felipe Vasconcelos <felipe.va@live.com>
2020-10-05 12:47:21 -0300

    Commit Inicial. Projeto refeito com adição do Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'atividade-semanal';
+  
+  public isCollapsedCcp = true;
+  public isCollapsedEng = true;
+  public isCollapsedMed = true;
+  public isCollapsedDir = true;
+  public isCollapsedAdm = true;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent,
+    ProfessoresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
